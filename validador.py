041a80721c0c73567aa8aa71293796189535cdec
joaquim oliveira <ti.automacao02@gscsc.com.br>
2025-07-25 15:08:29 -0300

    fix: add validador

---
+++ validador.py
@@ -0,0 +1,87 @@
+import os
+import openpyxl
+from datetime import datetime
+from dotenv import load_dotenv
+import pandas as pd
+
+#função validador_horario verifica o horario da primeira linha da coluna na planilha base.
+#caso esteja atrasado (dia anterior) apresenta um erro e pede para que a planilha base seja atualizada
+#caso contrario, segue executando o codigo.
+
+load_dotenv()
+def validador_horario(caminho_arquivo, nome_aba, coluna):
+    
+    try: 
+        workbook = openpyxl.load_workbook(caminho_arquivo, data_only=True)
+        sheet = workbook[nome_aba]
+        data_atual = datetime.now().date()
+        datas_invalidas = []
+
+        for row in range(2, sheet.max_row + 1):
+            try:
+                celula = sheet[f"{coluna}{row}"]
+                valor_celula = celula.value
+
+                if valor_celula is None:
+                    continue  
+                if isinstance(valor_celula, datetime):
+                    data_celula = valor_celula.date()
+                elif isinstance(valor_celula, str):
+                    
+                    try:
+                        data_celula = datetime.strptime(valor_celula.strip(), '%d/%m/%Y').date()
+                    except ValueError:
+                        try:
+                            data_celula = datetime.strptime(valor_celula.strip(), '%Y-%m-%d').date()
+                        except ValueError:
+                            raise ValueError("Formato desconhecido")
+                else:
+                    raise TypeError("Tipo de dado não reconhecido")
+
+                if data_celula != data_atual:
+                    datas_invalidas.append((row, data_celula))
+
+            except (ValueError, TypeError):
+                datas_invalidas.append((row, "Formato inválido"))
+
+        return datas_invalidas
+
+    except FileNotFoundError:
+        print(f"Erro: arquivo não encontrado em {caminho_arquivo}")
+        return None
+    except KeyError:
+        print(f"Erro: Aba '{nome_aba}' não encontrada no arquivo.")
+        return None
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
+        return None
+
+caminho = os.getenv('caminho_validador')
+aba = "Consolidado"
+coluna = "U"
+
+datas_invalidas = validador_horario(caminho, aba, coluna)
+
+if datas_invalidas is not None:
+    if datas_invalidas:
+        erros = []
+        print("Primeira linha com data atrasada encontrada:")
+        linha, data = datas_invalidas[0]
+        print(f"Linha {linha}: {data}")
+        msg = "Erro: planilha BASE não está atualizada!!!!!!"
+        print(msg)
+        erros.append({
+            "Pasta": "BASE",
+            "Erro": msg,
+            "DataHora": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        })
+        if erros:
+            df_erros = pd.DataFrame(erros)
+            log_path = os.path.join(os.getcwd(), "log_geral.xlsx")
+            df_erros.to_excel(log_path, index=False)
+            print(f"\nLog de erros da planilha geral salvo em: {log_path}")
+        else:
+            print("\n A pasta geral foi atualizada com sucesso!")
+        exit()
+    
+
