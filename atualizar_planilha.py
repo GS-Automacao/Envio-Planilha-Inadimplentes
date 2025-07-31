007c460309d8be633319d0529e7a43f522db310a
joaquim oliveira <ti.automacao02@gscsc.com.br>
2025-07-31 10:03:57 -0300

    v1.9.1 - fix
    modificando a função de atualizar a planilha

--- atualizar_planilha.py
+++ atualizar_planilha.py
@@ -2,6 +2,7 @@ import os
 import time
 import win32com.client
 from dotenv import load_dotenv
+import pythoncom
 
 load_dotenv()
 def atualizar_planilha_geral():
@@ -9,12 +10,14 @@ def atualizar_planilha_geral():
     excel.Visible = False
     excel.DisplayAlerts = False
     plan_geral = os.getenv('caminho_geral')
+    pythoncom.CoInitialize()
+
     try:
         plan_atualizada = excel.Workbooks.Open(os.path.abspath(plan_geral))
         plan_atualizada.RefreshAll()
         time.sleep(5) 
         plan_atualizada.Save()
-        plan_atualizada.Close()
+        plan_atualizada.Close(SaveChanges=True)
     except Exception as e:
         print(f"erro ao atualizar a planilha geral: {e}")
     finally:
@@ -25,12 +28,13 @@ def atualizar_planilha_excel(caminho_excel: str):
     excel = win32com.client.Dispatch("Excel.Application")
     excel.DisplayAlerts = False
     excel.Visible = False
+    pythoncom.CoInitialize()
     try:
         wb = excel.Workbooks.Open(os.path.abspath(caminho_excel))
         wb.RefreshAll()
         time.sleep(5)
         wb.Save()
-        wb.Close()
+        wb.Close(SaveChanges=True)
     except Exception as e:
         print(f"Erro ao atualizar a planilha: {caminho_excel}\n{e}")
     finally:
